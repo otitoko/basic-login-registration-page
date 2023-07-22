925b0c0976909e1daeed6336eb7e6cd036b28d6f
otitoko <tbkopera@proton.me>
2023-07-22 17:30:16 +0100

    Created registration system, fixed trailing new line issue

--- login.c
+++ login.c
@@ -1,53 +1,76 @@
 #include <stdio.h>
-#include <stdbool.h>
-//test est
+#include <string.h>
+//**
+//0. REIGSTRATION WORKS	 
+//1. upon registration, check for duplicate usernames
+//2. upon login, check for username and for matching password
+//3. hashing+encryption
+//
+//
+//
+//
+//notes: how to store the info? linked list? check data algorithms
+//
+//
+//
 char login_or_register;
-bool new_user;
+
+
 char username[20];
 char password[64];
 
 void printInitialPrompt();
+void login();
+void signIn();
+void register_to_file();
 
 int main(){
 	printInitialPrompt();
-	scanf("%c",&new_user);
+	scanf("%c",&login_or_register);
+	getchar();
 	if(login_or_register=='l'){
-		new_user=false;
+		login();
+	}
+	else if(login_or_register=='r'){
+		signIn();
 	}
 	else{
-		new_user=true;
-	}
+		printf("Please enter a valid choice\n");
+		printInitialPrompt();
 
 	return 0;
-
-
+	}
 }
-
 
 
 void printInitialPrompt(){
 	printf("Welcome to the Otitoko Login and Registration service.\n");
 	printf("Would you like to login or register?\n");
-	printf("(L)ogin					(R)egister");
+	printf("(L)ogin					(R)egister\n");
 }
 void login(){
 	printf("Enter your username: ");
-	scanf("%c",&username);
+	fgets(username, sizeof(username),stdin);
 	printf("\nEnter your password: ");
-	scanf("%c",&password);
+	fgets(password,sizeof(password),stdin);
 }
 void signIn(){
 	printf("Enter your username: ");
-	scanf("%c",&username);
+	fgets(username, sizeof(username),stdin);
+	username[strcspn(username, "\n")]= '\0';
+
 	printf("\nEnter your password: ");
-	scanf("%c",&password);
+	fgets(password, sizeof(password),stdin);
+	password[strcspn(password, "\n")]= '\0';
+
+	register_to_file(username,password);
 }
 
 void register_to_file(){
 	FILE *fileptr;
 
 	fileptr = fopen("private-data.txt","a");
-	fprintf(fileptr, "%c,%c\n",&username,&password);
+	fprintf(fileptr, "%s,%s\n",&username,&password);
 
 	fclose(fileptr);
 }
